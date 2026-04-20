49430affd593e12334a9e6f48aaea9c48814dad1
phamkhoaa06 <phamkhoaa2502@gmail.com>
2026-04-20 07:45:27 +0700

    Luyen tap 1 - Chuong

---
+++ Luyen tap 1-Chuong 2/bai 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14159;
+
+    // Mặc định 6 chữ số thập phân
+    printf("Mac dinh: %f\n", pi);
+
+    // Lấy 1 chữ số thập phân
+    printf("1 chu so thap phan: %.1f\n", pi);
+
+    // Lấy 4 chữ số thập phân
+    printf("4 chu so thap phan: %.4f\n", pi);
+
+    return 0;
+}
+// Created by khoa2 on 4/19/2026.
+//
---
+++ Luyen tap 1-Chuong 2/bai 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    const int soNguyen = 10;
+    const float soThuc = 3.14;
+    const char chuoi[] = "Hello C";
+
+    printf("So nguyen: %d\n", soNguyen);
+    printf("So thuc: %.2f\n", soThuc);
+    printf("Chuoi: %s\n", chuoi);
+
+    return 0;
+}
+// Created by khoa2 on 4/19/2026.
+//
